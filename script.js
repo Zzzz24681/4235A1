03cb5ac93c81577e66ad8f34f51830218b2c5a15
Zzzz24681 <153149934+Zzzz24681@users.noreply.github.com>
2024-07-01 17:27:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function adjustFontSize() {
   const baseSize = 12;
   const currentWidth = window.innerWidth;
-  const newSize = baseSize * (currentWidth / 2400);
+  const newSize = baseSize * (currentWidth / 1500);
   document.documentElement.style.fontSize = `${newSize}px`;
 }
 
